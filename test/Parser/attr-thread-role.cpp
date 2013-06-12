f149b536da89da7b6cef55d66c774c8e642dd393
Dean F. Sutherland <dsutherland@cert.org>
2013-06-12 12:03:19 -0400

    Rewrote thread-role annotations to more nearly resemble a typical use-case.

--- test/Parser/attr-thread-role.cpp
+++ test/Parser/attr-thread-role.cpp
@@ -1,14 +1,19 @@
 // RUN: %clang_cc1 %s -std=c++11 -fsyntax-only -thrd-role-analysis -verify
 // expected-no-diagnostics
 
-[[cert::thrd_role_decl("GUI")]]
+[[cert::thrd_role_decl("GUI, Worker")]]
+[[cert::thrd_role_incompatible("GUI, Worker")]]
+[[cert::thrd_role("GUI | Worker")]]
 void func() {
-  [[cert::thrd_role_revoke("Worker")]] [[cert::thrd_role_grant("Worker")]] {
+  [[cert::thrd_role_revoke("GUI")]] [[cert::thrd_role_grant("Worker")]] {
   }
 }
 
+[[cert::thrd_role("GUI | Worker")]]
 void func2() {
-  if (true) [[cert::thrd_role_grant("Worker")]] {
-  } else [[cert::thrd_role_grant("GUI")]] {
+  if (true) [[cert::thrd_role_revoke("GUI")]] 
+	    [[cert::thrd_role_grant("Worker")]] {
+  } else [[cert::thrd_role_revoke("Worker")]] 
+         [[cert::thrd_role_grant("GUI")]] {
   }
 }
