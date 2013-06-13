b169ed03a9b31a925ede051f3e73d2249d4e742d
Dean F. Sutherland <dsutherland@cert.org>
2013-06-13 14:05:11 -0400

    More torture tests

--- test/Sema/attr-thread-role.c
+++ test/Sema/attr-thread-role.c
@@ -4,5 +4,5 @@
 [[cert::thread_role_decl(12)]] int c;	// expected-error {{'thread_role_decl' attribute requires parameter 1 to be a string}}
 [[cert::thread_role_decl("")]] int d;     // expected-error {{cert::'thread_role_decl' requires a non-empty list of thread roles}}
 [[cert::thread_role_decl("GUI, Compute, GUI, Compute")]] int e; // expected-error {{duplicate name 'GUI' found in cert::'thread_role_decl'}} expected-error {{duplicate name 'Compute' found in cert::'thread_role_decl'}}
-[[cert::thread_role_decl()]] int f;	// expected-error {{expected expression}}
-[[cert::thread_role_decl(" , ")]] int g; 
+[[cert::thread_role_decl()]] int f;	// expected-error {{expected expression}} expected-error {{attribute takes one argument}}
+[[cert::thread_role_decl(" , ")]] int g; // expected-error {{Malformed thread role name '' found in cert::'thread_role_decl'}} expected-error {{Malformed thread role name '' found in cert::'thread_role_decl'}}
